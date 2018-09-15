d532b9b0d033cdb5537fc10564387de09f877bf4
bbarisic17 <bojan.barisic17@gmail.com>
2018-09-15 09:16:14 +0200

    Izmjena baze podataka

---
+++ Kladionica/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Kladionica.Model;
+using Kladionica.Services;
+using Kladionica.Servis;
+
+namespace Kladionica
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+#endregion
+        #region Fields
+        private List<Par> _parovi;
+        private DohvatPodataka dohvatPodataka = new DohvatPodataka();
+        private SpremanjePodataka spremanjePodataka = new SpremanjePodataka();
+        private Novcanik _novcanik;                
+        Listic _odigraniListic = new Listic();
+        #endregion
+        #region Events
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+           PostaviPocetnePodatke();                      
+        }
+                             
+        private void grdNogomet_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            int rowIndex = e.RowIndex;
+            DataGridViewRow selectedRow = grdNogomet.Rows[rowIndex];
+            Par odabraniPar = (Par)grdNogomet.CurrentRow.DataBoundItem;
+            if (odabraniPar == null)
+                return;
+                       
+            if (grdNogomet.Columns[e.ColumnIndex].Name == "Izbor1" && (bool)selectedRow.Cells[e.ColumnIndex].Value)
+            {
+                odabraniPar.IzborNerijeseno = false;
+                odabraniPar.IzborPobjedaGosta = false;
+                
+                selectedRow.Cells["Izbor2"].Value = false;
+                selectedRow.Cells["IzborX"].Value = false; 
+               
+                selectedRow.DefaultCellStyle.BackColor = Color.Aqua;
+            }
+            else if (grdNogomet.Columns[e.ColumnIndex].Name == "Izbor2" && (bool)selectedRow.Cells[e.ColumnIndex].Value)
+            {
+                selectedRow.Cells["Izbor1"].Value = false;
+                selectedRow.Cells["IzborX"].Value = false;
+                
+                odabraniPar.IzborPobjedaDomacina = false;
+                odabraniPar.IzborNerijeseno = false;
+                selectedRow.DefaultCellStyle.BackColor = Color.Aqua;
+            }
+            else if (grdNogomet.Columns[e.ColumnIndex].Name == "IzborX" && (bool)selectedRow.Cells[e.ColumnIndex].Value)
+            {
+                selectedRow.Cells["Izbor1"].Value = false;
+                selectedRow.Cells["Izbor2"].Value = false;
+                
+                odabraniPar.IzborPobjedaDomacina = false;
+                odabraniPar.IzborPobjedaGosta = false;
+                selectedRow.DefaultCellStyle.BackColor = Color.Aqua;
+            }
+            else
+                selectedRow.DefaultCellStyle.BackColor = Color.White;
+            ProvjeriStanjeListeIzabranihParova(odabraniPar);
+            IzracunajUkupnuKvotuDobitak();
+        }
+               
+        private void grdNogomet_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            grdNogomet.EndEdit();
+        }
+        private void textBoxUlog_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && (Keys)e.KeyChar != Keys.Back && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }            
+        }
+        private void textBoxUlog_TextChanged(object sender, EventArgs e)
+        {
+            IzracunajUkupnuKvotuDobitak();
+        }
+        #endregion
+
+        #region Methods
+        private void ProvjeriStanjeListeIzabranihParova(Par odabraniPar)
+        {
+            var parUListiIzabranihParova = _odigraniListic.ParoviNaListicu.FirstOrDefault(item => item.Id == odabraniPar.Id);
+            if (parUListiIzabranihParova == null)
+            {
+                if (odabraniPar.IzborPobjedaDomacina || odabraniPar.IzborPobjedaGosta || odabraniPar.IzborNerijeseno)
+                    _odigraniListic.ParoviNaListicu.Add(odabraniPar);
+            }
+            else if (parUListiIzabranihParova != null)
+            {
+                if (odabraniPar.IzborPobjedaDomacina || odabraniPar.IzborPobjedaGosta || odabraniPar.IzborNerijeseno)
+                    parUListiIzabranihParova = odabraniPar;
+                else
+                    _odigraniListic.ParoviNaListicu.Remove(odabraniPar);
+            }
+        }
+
+        private void IzracunajUkupnuKvotuDobitak()
+        {
+            _odigraniListic.UkupnaKvotaListica = 0m;
+            _odigraniListic.UkupniDobitakListica = 0m;
+            int brojacVrstaSporta = 0;
+            Dictionary<int, int> brojParovaIzSporta = new Dictionary<int, int>();
+            if (_odigraniListic.ParoviNaListicu != null)
+            {
+                foreach (var par in _odigraniListic.ParoviNaListicu)
+                {
+                    if (par.IzborPobjedaDomacina)
+                        _odigraniListic.UkupnaKvotaListica += par.KvotaPobjedaDomacina;
+                    else if (par.IzborPobjedaGosta)
+                        _odigraniListic.UkupnaKvotaListica += par.KvotaPobjedaGosta;
+                    else if (par.IzborNerijeseno)
+                        _odigraniListic.UkupnaKvotaListica += par.KvotaNerijeseno;
+
+                    if (brojParovaIzSporta.ContainsKey((int)par.VrstaSporta))
+                    {
+                        brojParovaIzSporta[(int)par.VrstaSporta] = brojParovaIzSporta[(int)par.VrstaSporta] + 1;
+                    }
+                    else
+                        brojParovaIzSporta.Add((int)par.VrstaSporta, 1);
+                }
+                foreach (var brojParova in brojParovaIzSporta)
+                {
+                    if (brojParova.Value >= 3)
+                        _odigraniListic.UkupnaKvotaListica += 5;
+                }
+                brojacVrstaSporta = Enum.GetNames(typeof(Enumeracije.VrstaSporta)).Length;
+
+                if (brojacVrstaSporta == brojParovaIzSporta.Count)
+                    _odigraniListic.UkupnaKvotaListica += 10;
+            }
+            if (!String.IsNullOrWhiteSpace(textBoxUlog.Text))
+            {
+                _odigraniListic.UplaceniIznos = decimal.Parse(textBoxUlog.Text.ToString());
+                _odigraniListic.UkupniDobitakListica = _odigraniListic.UplaceniIznos * _odigraniListic.UkupnaKvotaListica;                
+            }
+            else
+            {
+                _odigraniListic.UplaceniIznos = 0;
+                _odigraniListic.UkupniDobitakListica = 0;
+            }
+
+            PrikaziPodatke();
+            
+        }
+        #endregion               
+
+        private void btnSpremiListic_Click(object sender, EventArgs e)
+        {
+            if (_odigraniListic != null)
+            {
+                if (_novcanik.TrenutniIznos <= 0)
+                    MessageBox.Show("Nemate novaca na računu!");
+
+                else if (_odigraniListic.UplaceniIznos > _novcanik.TrenutniIznos)
+                    MessageBox.Show("Ulog ne smije biti veći od stanja na računu");
+
+                else if (_odigraniListic.ParoviNaListicu.Count == 0)                
+                    MessageBox.Show("Morate odabrati parove za listić!");
+
+                else if (_odigraniListic.UplaceniIznos <= 0)
+                    MessageBox.Show("Morate unijeti ulog!");
+
+                else
+                {
+                    _novcanik.TrenutniIznos -= _odigraniListic.UplaceniIznos;
+                    spremanjePodataka.SpremiStanjeNovcanika(_novcanik);
+
+                    spremanjePodataka.SpremiOdigraniListic(_odigraniListic);
+                    _odigraniListic = new Listic();
+                    PostaviPocetnePodatke();
+                }
+            }
+            
+        }
+        private void PrikaziPodatke()
+        {
+            textBoxUkupnaKvota.Text = _odigraniListic.UkupnaKvotaListica.ToString();
+            textBoxUkupniDobitak.Text = _odigraniListic.UkupniDobitakListica.ToString();
+            textBoxStanjeRacuna.Text = _novcanik.TrenutniIznos.ToString();            
+        }
+        private void PostaviPocetnePodatke()
+        {
+            _parovi = dohvatPodataka.DohvatiParove();
+            _novcanik = dohvatPodataka.DohvatiNovcanik();
+            textBoxUkupnaKvota.Text = _odigraniListic.UkupnaKvotaListica.ToString();
+            textBoxUkupniDobitak.Text = _odigraniListic.UkupniDobitakListica.ToString();
+            textBoxStanjeRacuna.Text = _novcanik.TrenutniIznos.ToString();
+            textBoxUlog.Text = _odigraniListic.UplaceniIznos.ToString();
+            grdNogomet.AutoGenerateColumns = false;
+            grdKosarka.AutoGenerateColumns = false;
+            grdRukomet.AutoGenerateColumns = false;
+            grdNogomet.DataSource = _parovi.Where(item => item.VrstaSporta == Enumeracije.VrstaSporta.Nogomet).ToList();
+            grdKosarka.DataSource = _parovi.Where(item => item.VrstaSporta == Enumeracije.VrstaSporta.Kosarka).ToList();
+            grdRukomet.DataSource = _parovi.Where(item => item.VrstaSporta == Enumeracije.VrstaSporta.Rukomet).ToList();
+        }
+    }
+}
---
+++ Kladionica/Services/SpremanjePodataka.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kladionica.Model;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace Kladionica.Services
+{
+    public class SpremanjePodataka
+    {
+        public void SpremiStanjeNovcanika(Novcanik novcanik)
+        {
+            string connectionString = PodaciZaKonekciju.DohvatiPodatkeZaKonekciju();
+            System.Data.SqlClient.SqlConnection sqlConnection =
+     new System.Data.SqlClient.SqlConnection(connectionString.ToString());
+
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+            cmd.CommandType = System.Data.CommandType.Text;            
+            cmd.CommandText = "UPDATE Novcanik SET [Iznos] = @Iznos WHERE [ID] = @ID";
+            cmd.Parameters.AddWithValue("@Iznos", novcanik.TrenutniIznos);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(novcanik.Id.ToString()));
+            cmd.Connection = sqlConnection;
+
+            sqlConnection.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+           
+        }
+        public void SpremiOdigraniListic(Listic listic)
+        {
+            string connectionString = PodaciZaKonekciju.DohvatiPodatkeZaKonekciju();
+            System.Data.SqlClient.SqlConnection sqlConnection =
+                new System.Data.SqlClient.SqlConnection(connectionString.ToString());
+
+            string sqlSpremanjeListica = @"INSERT INTO Listic ([Sifra], [UplaceniIznos], [UkupnaKvota], [UkupniDobitak])
+                                                VALUES (@Sifra, @UplaceniIznos, @UkupnaKvota, @UkupniDobitak) SELECT SCOPE_IDENTITY()";
+
+            string sqlSpremanjeParovaNaListicu = @"INSERT INTO Listic_Par ([ListicID], [ParID]) VALUES (@ListicId, @ParID)";
+
+            string sqlDohvatiZadnjuSifruListica = "SELECT TOP 1 Sifra from Listic order by ID desc";
+            string sqlDohvatiBrojListica = "SELECT COUNT (*) FROM Listic";
+
+            if (listic.Id == 0)
+            {
+                try
+                {                    
+                    System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+                    SqlDataReader reader = null;
+                    cmd.Connection = sqlConnection;
+                    sqlConnection.Open();
+
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = sqlDohvatiBrojListica;
+                    var brojListica = cmd.ExecuteScalar();
+                    if (brojListica == null)
+                        return;
+
+                    if (brojListica != null && int.Parse(brojListica.ToString()) == 0)
+                    {
+                        listic.Sifra = "00001";
+                    }
+                    else
+                    {
+                        cmd.CommandText = sqlDohvatiZadnjuSifruListica;
+                        reader = cmd.ExecuteReader();
+                        string zadnjaSifra = String.Empty;
+                        while (reader.Read())
+                        {
+                            zadnjaSifra = reader["Sifra"].ToString();
+                        }
+                        if (!String.IsNullOrWhiteSpace(zadnjaSifra))
+                        {
+                            int zadnjaSifraTemp = int.Parse(zadnjaSifra.ToString());
+                            if (zadnjaSifraTemp != 0)
+                            {
+                                zadnjaSifraTemp += 1;
+                                listic.Sifra = zadnjaSifraTemp.ToString("D5");
+                            }
+                        }
+                        else
+                            return;
+                        reader.Close();
+                    }
+
+                    cmd.CommandText = sqlSpremanjeListica;
+                    cmd.Parameters.AddWithValue("@Sifra", listic.Sifra);
+                    cmd.Parameters.AddWithValue("@UplaceniIznos", listic.UplaceniIznos);
+                    cmd.Parameters.AddWithValue("@UkupnaKvota", listic.UkupnaKvotaListica);
+                    cmd.Parameters.AddWithValue("@UkupniDobitak", listic.UkupniDobitakListica);
+                    
+                    var lastId = cmd.ExecuteScalar();
+                    if (lastId != null)
+                        listic.Id = uint.Parse(lastId.ToString());
+
+                    if (listic.ParoviNaListicu != null && listic.ParoviNaListicu.Count > 0)
+                    {
+                        cmd.CommandText = sqlSpremanjeParovaNaListicu;
+                        foreach (var par in listic.ParoviNaListicu)
+                        {
+                            cmd.Parameters.Clear();
+                            cmd.Parameters.AddWithValue("@ListicID", int.Parse(listic.Id.ToString()));
+                            cmd.Parameters.AddWithValue("ParID", int.Parse(par.Id.ToString()));
+                            cmd.ExecuteNonQuery();
+                        }                        
+                    }
+                    sqlConnection.Close();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public void UnosTestnihPodataka()
+        {
+            string connectionString = PodaciZaKonekciju.DohvatiPodatkeZaKonekciju();
+            System.Data.SqlClient.SqlConnection sqlConnection =
+                new System.Data.SqlClient.SqlConnection(connectionString.ToString());
+
+            string sqlProvjeriPodatkeNovcanika = "SELECT COUNT (*) FROM Novcanik";
+            string sqlProvjeriPodatkeParova = "SELECT COUNT (*) FROM Par";
+            string sqlUnosPodatakaNovcanika = "INSERT INTO Novcanik ([Iznos]) VALUES (100)";
+            string sqlUnosParova = @"INSERT INTO Par ([Domacin], [Gost], [VrstaSporta], [KvotaPobjedaDomacina], [KvotaPobjedaGosta], [KvotaNerijeseno])
+                        VALUES ('Leicester', 'Liverpool', 1, 2.10, 1.50, 1.35),
+                        ('Crystal Palace', 'Southempton', 1, 1.10, 2.70, 1.90),
+                        ('Everton', 'Huddersfield', 1, 2.10, 1.50, 1.35),
+                        ('Brighton', 'Fulham', 1, 1.80, 2.20, 1.35),
+                        ('Chelsea', 'Bournemouth', 1, 1.10, 3.50, 2.35),
+                        ('West Ham', 'Wolverhampton', 1, 1.60, 1.90, 1.65),
+                        ('Man City', 'Newcastle', 1, 1.10, 2.50, 1.80)";
+
+            try
+            {
+                System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+                cmd.Connection = sqlConnection;
+                sqlConnection.Open();
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = sqlProvjeriPodatkeNovcanika;
+                var podaci = cmd.ExecuteScalar();
+                if (podaci == null)
+                    return;
+
+                else if (podaci != null && int.Parse(podaci.ToString()) == 0)
+                {
+                    cmd.CommandText = sqlUnosPodatakaNovcanika;
+                    cmd.ExecuteNonQuery();                    
+                }
+
+                cmd.CommandText = sqlProvjeriPodatkeParova;
+                var brojParova = cmd.ExecuteScalar();
+                if (brojParova == null)
+                    return;
+                else if (brojParova != null && int.Parse(brojParova.ToString()) == 0)
+                {
+                    cmd.CommandText = sqlUnosParova;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+}
